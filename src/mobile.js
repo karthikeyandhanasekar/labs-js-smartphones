f3982571bb9f1725d442acd9e22b7b61ac2cf6ae
Karthik <dkkarthik2000@gmail.com>
2021-10-12 09:03:43 +0530

    Completed

--- src/mobile.js
+++ src/mobile.js
@@ -1,108 +1,90 @@
-//SmartPhone
 class SmartPhone
 {
     constructor(ram,batteryPower,price)
     {
-        this.ram=ram
-        this,batteryPower= batteryPower
-        this.price=price
+        this.ram=ram;
+        this.batteryPower=batteryPower;
+        this.price=price;
     }
     displayFeatures()
     {
-        return this.ram + "\n" + this.battery + "\n" + this.price
+        return this.ram + "\n" + this.batteryPower + "\n" + this.price;
     }
     remainingAmount(amountIhave)
     {
-        
-        if (this.price >= amountIhave)
-        return 'Customer has no more amount'
-        else if (amountIhave > this.price)
-        {
-            amountIhave = amountIhave-this.price
-            return
-        }
-        return amountIhave
-       
+        this.price = this.price -amountIhave
+
     }
 }
 
-
-// AndroidPhone
-
-class AndroidPhone
+class AndroidPhone extends SmartPhone
 {
-    constructor(modelName, ram, batteryPower, price) {
-        this.modelName =modelName
-        this.ram=ram
-        this.price=price
-        this.batteryPower=batteryPower
+    constructor(modelName,ram,batteryPower,price)
+    {
+        super(ram,batteryPower,price);
+        this.modelName=modelName;
     }
     displayFeatures()
     {
-        return this.modelName+ "\n"+ this.ram + "\n" + this.batteryPower + "\n" + this.price
+        return  this.modelName + "\n"+ this.ram + "\n" + this.batteryPower + "\n" + this.price;
     }
     remainingAmount(amountIhave)
     {
-        if (this.price >= amountIhave)
-        return 'Customer has no more amount'
-        else if (amountIhave > this.price)
-        {
-        this.price = Math.abs(this.price - amountIhave)
-        return 'Customer has remaining Rs.'+this.price+' after buying the android phone'
-        }
+        this.price =amountIhave - this.price
+        if (this.price>0)
+        return "Customer has remaining Rs."+ this.price  +" after buying the android phone"
         else
-        {
-        return amountIhave-this.price
-        }
+        return "Customer has no more amount"
+
+
+
     }
+
 }
 
-// IPhone
-
-class IPhone
+class IPhone extends SmartPhone
 {
-    constructor(seriesName, ram, batteryPower, price) {
-        this.seriesName =seriesName
-        this.ram=ram
-        this.price=price
-        this.batteryPower=batteryPower
+    constructor(seriesName,ram,batteryPower,price)
+    {
+        super(ram,batteryPower,price);
+        this.seriesName=seriesName;
     }
     displayFeatures()
     {
-        return this.seriesName+ "\n"+ this.ram + "\n" + this.batteryPower + "\n" + this.price
+        return  this.seriesName + "\n"+ this.ram + "\n" + this.batteryPower + "\n" + this.price;
     }
     remainingAmount(amountIhave)
     {
         if (this.price > amountIhave)
         return "Customer can't able to buy a phone due to insufficient amount"
-        else if (this.price === amountIhave)
+
+        this.price =amountIhave - this.price
+        if (this.price <=0)
         return "Customer has no more amount"
-        else if (amountIhave > this.price)
-        {
-        this.price = Math.abs(this.price - amountIhave)
-        return 'Customer has remaining Rs.'+this.price+' after buying an iphone'
-        }
-    
         else
-        {
-        return amountIhave-this.price
-        }
+        return "Customer has remaining Rs."+ this.price  +" after buying an iphone"
+
+
+
+
+
     }
+
 }
 
-
-
-//MobileCampus
 class MobileCampus
 {
-    constructor(NoOfMobiles,NoOfAndroidMobiles,NoOfIPhoneMobiles)
+    constructor(NoOfMobiles, NoOfAndroidMobiles , NoOfIPhoneMobiles)
     {
-        this.NoOfMobiles=NoOfMobiles
-        this.NoOfAndroidMobiles = NoOfAndroidMobiles
+        this.NoOfMobiles = NoOfMobiles;
+
+        this.NoOfAndroidMobiles = NoOfAndroidMobiles;
         this.NoOfIPhoneMobiles = NoOfIPhoneMobiles
     }
     changeMobileAvailabilityNumber(count,type)
     {
+        this.NoOfMobiles=this.NoOfMobiles-count;
+
         if(type === 'android'){
             this.NoOfAndroidMobiles = this.NoOfAndroidMobiles - count;
             return 'Available android mobiles are '+this.NoOfAndroidMobiles;
@@ -112,6 +94,6 @@ class MobileCampus
             this.NoOfIPhoneMobiles = this.NoOfIPhoneMobiles - count;
             return 'Available iphone mobiles are '+this.NoOfIPhoneMobiles;
         }
-        return this.NoOfMobiles -count
     }
+    
 }
